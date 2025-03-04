e7628bcdc8258031933f7c2065f902f91e292b6e
Robin Wieruch <hello@rwieruch.com>
2025-03-04 14:03:13 +0100

    fix(cookies): in StrictMode, the toast would be shown twice

--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -2,16 +2,15 @@
 
 import { useEffect } from "react";
 import { toast } from "sonner";
-import { deleteCookieByKey, getCookieByKey } from "@/actions/cookies";
+import { consumeCookiedByKey } from "@/actions/cookies";
 
 const RedirectToast = () => {
   useEffect(() => {
     const showCookieToast = async () => {
-      const message = await getCookieByKey("toast");
+      const message = await consumeCookiedByKey("toast");
 
       if (message) {
         toast.success(message);
-        await deleteCookieByKey("toast");
       }
     };
 
