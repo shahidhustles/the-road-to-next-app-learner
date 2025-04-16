2ee36368a56dbac761ee2d3bb3754ca79ee7ed56
Robin Wieruch <hello@rwieruch.com>
2025-04-16 22:53:11 +0200

    fix(2024-11_cursor-based-pagination_deliberate-over-fetching): 2024-11_cursor-based-pagination_deliberate-over-fetching

--- src/features/comment/queries/get-comments.ts
+++ src/features/comment/queries/get-comments.ts
@@ -19,10 +19,11 @@ export const getComments = async (
 
   const take = 2;
 
-  const [comments, count] = await prisma.$transaction([
+  // eslint-disable-next-line prefer-const
+  let [comments, count] = await prisma.$transaction([
     prisma.comment.findMany({
       where,
-      take,
+      take: take + 1,
       cursor: cursor
         ? { createdAt: new Date(cursor.createdAt), id: cursor.id }
         : undefined,
@@ -41,7 +42,8 @@ export const getComments = async (
     }),
   ]);
 
-  const hasNextPage = true;
+  const hasNextPage = comments.length > take;
+  comments = hasNextPage ? comments.slice(0, -1) : comments;
 
   const lastComment = comments.at(-1);
 
